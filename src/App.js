b49d9453275cf6f270e26a40f37bf7862b722f9b
Brian <briantcheung@gmail.com>
2024-07-21 14:45:26 -0700

    Added page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Autocomplete, SvgIcon, TextField } from "@mui/material"
+
+const labels = [
+    {
+        label: "One",
+        value: 1
+    },
+    {
+        label: "Two",
+        value: 2
+    }
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Autocomplete
+            sx={{ mt: "30px", ml: "30px", width: 120 }}
+            options={labels}
+            value={labels[0]}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Autocomplete"
+                    InputProps={{
+                        ...params.InputProps,
+                        startAdornment: (
+                            <SvgIcon>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4.5 12a7.5 7.5 0 0015 0m-15 0a7.5 7.5 0 1115 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077l1.41-.513m14.095-5.13l1.41-.513M5.106 17.785l1.15-.964m11.49-9.642l1.149-.964M7.501 19.795l.75-1.3m7.5-12.99l.75-1.3m-6.063 16.658l.26-1.477m2.605-14.772l.26-1.477m0 17.726l-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205L12 12m6.894 5.785l-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864l-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495"
+                                    />
+                                </svg>
+                            </SvgIcon>
+                        ),
+                    }}
+                />
+            )}
+        />
+    )
 }
 
-export default App;
+export default App
